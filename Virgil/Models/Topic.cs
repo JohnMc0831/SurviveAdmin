b18c7688d5e0f8a09428e2798747e6bb8c9a3518
John McConnell <johnmc0831@gmail.com>
2017-02-19 10:20:26 -0500

    Updates to support multi-language

--- Virgil/Models/Topic.cs
+++ Virgil/Models/Topic.cs
@@ -16,10 +16,14 @@ namespace Virgil.Models
         public int id { get; set; }
         [Required]
         public string Title { get; set; }
+        public string TitleGerman { get; set; }
         public string Summary { get; set; }
+        public string SummaryGerman { get; set; }
         [Required]
         [AllowHtml]
         public string Body { get; set; }
+        [AllowHtml]
+        public string BodyGerman { get; set; }
         public string BackColor { get; set; }
         public string TextColor { get; set; }
         [Display(Name="Display Order")]
